7ffb2be9e0bd30c5a789b3b6e48248eb72255a73
Joseph Utecht <thenmal@gmail.com>
2012-09-19 10:53:45 -0500

    converted project to south

--- Wob/settings.py
+++ Wob/settings.py
@@ -121,7 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'polls',
     'demo',
-    #'south',
+    'south',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
