90455b7f454e878773fa67ea972fc3a1fac46f48
Joseph Utecht <thenmal@gmail.com>
2012-09-21 15:46:44 -0500

    fixed admin, moved it to demo

---
+++ demo/admin.py
@@ -0,0 +1,8 @@
+from demo.models import *
+from django.contrib import admin
+
+
+admin.site.register(Event)
+admin.site.register(Character)
+admin.site.register(Player)
+admin.site.register(Article)
