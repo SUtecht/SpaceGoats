746cae720fc4a8c8c9d61618132e42c9ca6c1965
Quasar Jarosz <quasar@ja.rosz.org>
2012-09-24 16:33:06 -0500

    battlenet character updating works now.
    Both in views and in jobs.

---
+++ demo/jobs/hourly/char_update.py
@@ -0,0 +1,14 @@
+from django_extensions.management.jobs import HourlyJob
+
+from demo.models import Character
+from demo.utils import update_character
+
+class Job(HourlyJob):
+    help = "Update character info."
+
+    def execute(self):
+        # executing empty sample job
+        print "Updating character info now."
+
+        for char in Character.objects.all():
+            update_character(char)
