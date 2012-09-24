74fd38729fd70083116a867d59c8f4d7a534c729
Quasar Jarosz <quasar@ja.rosz.org>
2012-09-24 16:04:17 -0500

    extending job to check battlenet

--- demo/jobs/hourly/sample.py
+++ demo/jobs/hourly/sample.py
@@ -1,13 +1,38 @@
 from django_extensions.management.jobs import HourlyJob
+import battlenet, battlenet.enums
+from battlenet import Connection
+from battlenet import Character as BnetChar
+
 from demo.models import Character
 
 class Job(HourlyJob):
     help = "My sample job."
+    connection = None
+
+
+    def get_info(self, char):
+        print "Getting info for: {}".format(char.name)
+        character = self.connection.get_character(battlenet.UNITED_STATES, 
+                                                  'Auchindoun', 
+                                                  char.name, 
+                                                  fields=[BnetChar.GUILD])
+
+        print battlenet.enums.CLASS[character.class_]
+        print character.level
+        print character.equipment.average_item_level
+
 
     def execute(self):
         # executing empty sample job
         print "Executing sample job now."
-        print Character.objects.all()
+
+        self.connection = Connection()
+
+
+        for char in Character.objects.all():
+            self.get_info(char)
+
+
 
 
 
