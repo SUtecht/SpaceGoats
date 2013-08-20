a98455156a3e17808ef2602f532f45bf61b1509b
Joseph Utecht <thenmal@gmail.com>
2013-08-20 16:26:39 -0500

    added ffxiv folder

---
+++ ffxiv/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django import forms
+from goatnails.db.models import ImageWithThumbsField
+
+#Characters
+# -Levels/Classes
+# -Name
+# -Picture
+# -User
+
+#Article
+# -Title
+# -text (parsed markdown)
+
+#Screenshot
+# -User
+# -Image
+# -Caption
