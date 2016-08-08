9c737520467101291f341fbdb804266efb76147e
Joseph Utecht <thenmal@gmail.com>
2016-08-08 15:27:11 -0500

    not equipped

--- demo/utils.py
+++ demo/utils.py
@@ -4,12 +4,12 @@ import requests
 def update_character(char):
     print "Getting info for: {}".format(char.name)
     try:
-        r = requests.get('https://us.api.battle.net/wow/character/{}/{}?fields=items&locale=en_US&apikey=zpwrz24xqd8yhmhk2uv8b4qr5gv6ecef'
+        r = requests.get('https://us.api.battle.net/wow/character/{}/{}?fields=items&locale=en_US&apikey=APIKEY'
                          .format(char.server, char.name))
         character = r.json()
 
         char.level = character['level']
-        char.ilvl = character['items']['averageItemLevelEquipped']
+        char.ilvl = character['items']['averageItemLevel']
 
         char.save()
     except:
